b1d8816fd1bb6c55268707d73f48622a96ca76df
DaryaYu <33330219+DaryaYu@users.noreply.github.com>
2020-02-27 21:37:04 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -18,13 +18,21 @@ def predict():
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
+    probability = model.predict_proba(final_features)
 
     if prediction[0] == 1:
         output = 'Yes'
+        probability = probability[0][1]
     else:
         output = 'No'
+        probability = probability[0][0]
 
-    return render_template('index.html', prediction_text='Will the person default on a next month payment? - {}'.format(output))
+    prediction_text_result = 'Will the person default on the next month payment?\n- {} with {:2.2%} probability'.format(output, probability)
+    
+    if request.is_xhr:
+        return jsonify({'prediction_text': prediction_text_result})
+    return render_template('index.html', prediction_text = prediction_text_result)
+
 
 if __name__ == "__main__":
     app.run()
